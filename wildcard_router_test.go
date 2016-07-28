bf3de61e80d2901802ebf94f051bd16b4dc3b16a
binku <binku87@gmail.com>
2016-07-28 16:21:18 +0800

    Add router testcases

---
+++ wildcard_router_test.go
@@ -0,0 +1,73 @@
+package wildcard_router_test
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/qor/wildcard_router"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+var (
+	mux    = http.NewServeMux()
+	Server = httptest.NewServer(mux)
+)
+
+type ModuleA struct {
+	wildcard_router.WildcardInterface
+}
+
+func (a ModuleA) Handle(w http.ResponseWriter, req *http.Request) bool {
+	if req.URL.Path == "/module_a" {
+		w.Write([]byte("Module A handled"))
+		return true
+	}
+	return false
+}
+
+type ModuleB struct {
+	wildcard_router.WildcardInterface
+}
+
+func (b ModuleB) Handle(w http.ResponseWriter, req *http.Request) bool {
+	if req.URL.Path == "/module_b" {
+		w.Write([]byte("Module B handled"))
+		return true
+	}
+	return false
+}
+
+func init() {
+	WildcardRouter := wildcard_router.New(mux)
+	WildcardRouter.AddHandler(ModuleA{})
+	WildcardRouter.AddHandler(ModuleB{})
+}
+
+type WildcardRouterTestCase struct {
+	URL              string
+	ExpectHasContent string
+}
+
+func TestWildcardRouter(t *testing.T) {
+	testCases := []WildcardRouterTestCase{
+		{URL: "/module_a", ExpectHasContent: "Module A handled"},
+		{URL: "/module_b", ExpectHasContent: "Module B handled"},
+		{URL: "/module_x", ExpectHasContent: "404 page not found"},
+	}
+
+	for i, testCase := range testCases {
+		var hasError bool
+		req, _ := http.Get(Server.URL + testCase.URL)
+		content, _ := ioutil.ReadAll(req.Body)
+		if !strings.Contains(string(content), testCase.ExpectHasContent) {
+			t.Errorf(color.RedString(fmt.Sprintf("WildcardRouter #%v: HTML expect have content '%v', but got '%v'", i+1, testCase.ExpectHasContent, string(content))))
+			hasError = true
+		}
+		if !hasError {
+			t.Errorf(color.GreenString(fmt.Sprintf("WildcardRouter #%v: Success", i+1)))
+		}
+	}
+}
